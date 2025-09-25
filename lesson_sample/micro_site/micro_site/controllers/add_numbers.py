ba5200d6dbbea8764d4e8a9e98b7e2986e163d79
toshiya <kmm2459371@stu.o-hara.ac.jp>
2025-09-25 12:21:38 +0900

    add_numbers

--- lesson_sample/micro_site/micro_site/controllers/add_numbers.py
+++ lesson_sample/micro_site/micro_site/controllers/add_numbers.py
@@ -1,9 +1,17 @@
-from utils import render_template
+from utils import parse_post, render_template
 from app_logic import set_first_value, set_second_value, get_addition
 
 
 def add_numbers(environ):
-    # ここに処理を書く
+    method = environ["REQUEST_METHOD"]
+    if method == "POST":
+        data = parse_post(environ)
+        first_value = data.get("first_value", [0])[0]
+        second_value = data.get("second_value", [0])[0]
+        set_first_value(first_value)
+        set_second_value(second_value)
+
+        #ここに処理を書く
     set_first_value(10)
     set_second_value(20)
 
