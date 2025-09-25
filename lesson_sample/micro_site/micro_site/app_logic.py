401ad75216898b439bced6036ba2bc1b520dfc87
toshiya <kmm2459371@stu.o-hara.ac.jp>
2025-09-25 11:49:50 +0900

    足し算

--- lesson_sample/micro_site/micro_site/app_logic.py
+++ lesson_sample/micro_site/micro_site/app_logic.py
@@ -45,6 +45,30 @@ def get_my_greeting() -> str:
     except (json.JSONDecodeError, OSError):
         return ""
 
-# set_first_value: first_valueをjsonに保存
-# set_second_value: second_valueをjsonに保存
-# get_addition: jsonから値を取ってきて、足し算した結果を返却
+
+def set_first_value(first_value) -> None:
+    """1つ目の値をJSONに保存"""
+    with open(ROBODOG_FILE, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    data['first_value'] = first_value
+    with open(ROBODOG_FILE, 'w', encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+
+def set_second_value(second_value) -> None:
+    """2つ目の値をJSONに保存"""
+    with open(ROBODOG_FILE, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    data['second_value'] = second_value
+    with open(ROBODOG_FILE, 'w', encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+
+def get_addition() -> int:
+    """足し算の結果を返す"""
+    with open(ROBODOG_FILE, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    addition = data['first_value'] + data['second_value']
+
+    return addition
